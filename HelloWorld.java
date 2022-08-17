00d4dd4c03fbb9be39a2466d654ca31c61c4870f
GAJENDRA-TANDEKAR <96012296+GAJENDRA-TANDEKAR@users.noreply.github.com>
2022-08-17 19:25:42 +0530

    h
    h

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.print("Sum of three numbers ");
+
+        int a = 2;
+        int b = 4;
+        int c = 4;
+        int d = a + b + c ;
+
+        System.out.println(d);
+    }
+}
